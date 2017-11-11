89bed23222e77b5d2405e116f0e0d525e1c172ad
uodna <uodna0@gmail.com>
2017-11-12 02:34:56 +0900

    osx-ansi v2

--- keyboards/ergodox_ez/keymaps/osx-ansi/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx-ansi/keymap.c
@@ -26,21 +26,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  `~    |  1!  |  2@  |  3#  |  4$  |  5%  | LEFT |           | RIGHT|  6^  |  7&  |  8*  |  9(  |  0)  |   -_   |
+ * |        |  1!  |  2@  |  3#  |  4$  |  5%  | LEFT |           | RIGHT|  6^  |  7&  |  8*  |  9(  |  0)  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   [{   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   -_   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   '"   |
- * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |   ]}   |
+ * | [{/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  | ]}/Ctrl|
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |'"/Shift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |=+/Alt| LGui |                                       | RGui |  \|  |      |      |      |
+ *   | Esc  |      |      | LAlt | LGui |                                       | RGui |  \|  |      |      |  Esc |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | BS   | LGui |       | Alt  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Del  |       | PgUp |        |      |
- *                                 | Space| Esc  |------|       |------|  Esc   |Enter |
+ *                                 | Space| `~   |------|       |------|  =+    |Enter |
  *                                 |      | /L1  | End  |       | PgDn |  /L1   |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -48,23 +48,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRV,     KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   S(KC_LBRC),
-        KC_LCTRL,      KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
-        KC_NO,KC_NO,     KC_NO,  LALT_T(KC_EQL),KC_LGUI,
+        KC_NO,     KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
+        LCTL_T(KC_LBRC),      KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
+        KC_ESC,KC_NO,     KC_NO,  KC_LALT,KC_LGUI,
                                               KC_BSPACE,  KC_LGUI,
                                                               KC_DEL,
-                                              KC_SPC,LT(SYMB,KC_ESC),KC_END,
+                                              KC_SPC,LT(SYMB,KC_GRV),KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             S(KC_RBRC),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_LBRC,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             KC_RBRC,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RBRC,
-                                  KC_RGUI,KC_BSLS,KC_NO,KC_NO,        KC_NO,
+             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_NO,
+             KC_NO,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
+                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          RCTL_T(KC_RBRC),
+             KC_NO,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          RSFT_T(KC_QUOT),
+                                  KC_RGUI,KC_BSLS,KC_NO,KC_NO,        KC_ESC,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
-             KC_PGDN,LT(SYMB,KC_ESC), KC_ENT
+             KC_PGDN,LT(SYMB,KC_EQL), KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
