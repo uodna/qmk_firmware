7b5970a47c686b8f42e8a1a507eb8b17af2138c8
uodna <uodna0@gmail.com>
2017-11-12 11:28:54 +0900

    Move Esc Key

--- keyboards/ergodox_ez/keymaps/osx-ansi/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx-ansi/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |'"/Shift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Esc  |      |      | LAlt | LGui |                                       | RGui |  \|  |      |      |  Esc |
+ *   |      |      |      |Esc/Alt| LGui|                                       | RGui |  \|  |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | BS   | LGui |       | Alt  |Ctrl/Esc|
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         LCTL_T(KC_LBRC),      KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
-        KC_ESC,KC_NO,     KC_NO,  KC_LALT,KC_LGUI,
+        KC_NO,KC_NO,     KC_NO,  LALT_T(KC_ESC),KC_LGUI,
                                               KC_BSPACE,  KC_LGUI,
                                                               KC_DEL,
                                               KC_SPC,LT(SYMB,KC_GRV),KC_END,
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_NO,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          RCTL_T(KC_RBRC),
              KC_NO,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          RSFT_T(KC_QUOT),
-                                  KC_RGUI,KC_BSLS,KC_NO,KC_NO,        KC_ESC,
+                                  KC_RGUI,KC_BSLS,KC_NO,KC_NO,        KC_NO,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,LT(SYMB,KC_EQL), KC_ENT
