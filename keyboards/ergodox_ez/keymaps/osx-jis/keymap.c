db138d1d2e3103cd81fb8e8750cb135c8d539d35
uodna <uodna0@gmail.com>
2017-11-19 22:07:25 +0900

    fix toglle layer keys

--- keyboards/ergodox_ez/keymaps/osx-jis/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx-jis/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_ESC,     KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MDIA,
+        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(MDIA),
         LCTL_T(JA_LBRC),      KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
         KC_NO,KC_NO,     KC_NO,  LALT_T(KC_ESC),KC_LGUI,
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                               KC_SPC,LT(SYMB,JA_HAT),KC_END,
         // right hand
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   M_ZR_AST,             KC_BSPACE,
-             SYMB,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
+             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          RCTL_T(JA_RBRC),
              KC_NO,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          M_CLN_UN,
                                   KC_RALT,JA_ENVL,KC_NO,KC_NO,        KC_NO,
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_ESC,     KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MDIA,
+        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(MDIA),
         KC_LCTL,      KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
         KC_NO,KC_NO,     KC_NO,  KC_LALT,KC_LGUI,
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                               KC_SPC,LT(SYMB,JA_HAT),KC_END,
         // right hand
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   M_ZR_AST,             KC_BSPACE,
-             SYMB,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
+             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          JA_RBRC,
              KC_NO,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          M_CLN_UN,
                                   KC_RGUI,JA_ENVL,KC_NO,KC_NO,        KC_NO,
