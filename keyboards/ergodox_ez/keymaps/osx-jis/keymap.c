9b83680d325b531fbaf5e495a0b89623902d3967
uodna <uodna0@gmail.com>
2017-11-27 21:25:06 +0900

    KC_RO

--- keyboards/ergodox_ez/keymaps/osx-jis/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx-jis/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;+  |   :*   |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /?  |   :_   |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /?  |   _    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      | Esc  | LGui |                                       | RCtrl|  \|  |      |      |      |
  *   |      |      |      |/LAlt |      |                                       |      |      |      |      |      |
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   M_ZR_AST,             KC_BSPACE,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          JA_CLON,
-             JA_RBRC,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          M_CLN_UN,
+             JA_RBRC,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RO,
                                   KC_RCTL,JA_ENVL,KC_NO,KC_NO,        KC_NO,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
