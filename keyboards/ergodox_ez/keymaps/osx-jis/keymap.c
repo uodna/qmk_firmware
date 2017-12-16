604c03d32f13b30d2b45ae894c12ac1f201a146d
uodna <uodna0@gmail.com>
2017-12-17 01:30:59 +0900

    remove redundant symbol bindings

--- keyboards/ergodox_ez/keymaps/osx-jis/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx-jis/keymap.c
@@ -29,12 +29,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |  1!  |  2"  |  3#  |  4$  |  5%  | LEFT |           | RIGHT|  6&  |  7'  |  8(  |  9)  |  0*  |   BS   |
+ * | Esc    |  1!  |  2"  |  3#  |  4$  |  5%  | LEFT |           | RIGHT|  6&  |  7'  |  8(  |  9)  |  0   |   BS   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   -=   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   -=   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;+  |   :*   |
- * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /?  |   _    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      | LAlt | LGui |                                       | KP1  |  \|  |      |      |      |
@@ -53,18 +53,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_ESC,     KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LSFT(JA_LBRC),
+        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         KC_LCTL,      KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   JA_LBRC,
+        KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
         KC_NO,KC_NO,     KC_NO,  KC_LALT,KC_LGUI,
                                               KC_BSPACE,  TG(MDIA),
                                                               KC_DEL,
                                               KC_SPC,KC_ESC,KC_END,
         // right hand
-             KC_RGHT, KC_6,   KC_7,  KC_8,   KC_9,   M_ZR_AST,             KC_BSPACE,
-             LSFT(JA_RBRC),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
+             KC_RGHT, KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPACE,
+             KC_NO,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_MINS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          JA_CLON,
-             JA_RBRC,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RO,
+             KC_NO,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RO,
                                   LT(SYMB,KC_P1),JA_ENVL,KC_NO,KC_NO,        KC_NO,
              TG(SYMB), CTL_T(KC_ESC),
              KC_PGUP,
