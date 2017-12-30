2a97ebe2abc32ca7303702d50aa8e9272bb6e4fc
uodna <uodna0@gmail.com>
2017-12-31 00:29:23 +0900

    move Esc and Meh

--- keyboards/ergodox_ez/keymaps/uodna-osx-jis/keymap.c
+++ keyboards/ergodox_ez/keymaps/uodna-osx-jis/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /?  |   _    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | LAlt | LGui |                                       | KP1  |  \|  |      |      |      |
- *   |      |      |      |      |      |                                       | /L1  |      |      |      |      |
+ *   |      |      |      | Esc  | LGui |                                       | KP1  |  \|  |      |      |      |
+ *   |      |      |      | /Alt |      |                                       | /L1  | /Meh |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | BS   |  L2  |       |  L1  |Ctrl/Esc|
@@ -57,22 +57,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,   KC_1,   KC_2,   KC_3,     KC_4,     KC_5,     KC_LEFT,
-        KC_TAB,   KC_Q,   KC_W,   KC_E,     KC_R,     KC_T,     KC_NO,
-        KC_LCTL,  KC_A,   KC_S,   KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,   KC_X,   KC_C,     KC_V,     KC_B,     KC_NO,
-        KC_NO,    KC_NO,  KC_NO,  KC_LALT,  KC_LGUI,
+        KC_ESC,   KC_1,   KC_2,   KC_3,           KC_4,     KC_5,     KC_LEFT,
+        KC_TAB,   KC_Q,   KC_W,   KC_E,           KC_R,     KC_T,     KC_NO,
+        KC_LCTL,  KC_A,   KC_S,   KC_D,           KC_F,     KC_G,
+        KC_LSFT,  KC_Z,   KC_X,   KC_C,           KC_V,     KC_B,     KC_NO,
+        KC_NO,    KC_NO,  KC_NO,  ALT_T(KC_ESC),  KC_LGUI,
 
                                                       KC_BSPC,  TG(MDIA),
                                                                 KC_DEL,
                                             KC_SPC,   KC_ESC,   KC_END,
 
         // right hand
-        KC_RGHT,  KC_6,            KC_7,     KC_8,    KC_9,    KC_0,     KC_BSPC,
-        KC_NO,    KC_Y,            KC_U,     KC_I,    KC_O,    KC_P,     KC_MINS,
-                  KC_H,            KC_J,     KC_K,    KC_L,    KC_SCLN,  JA_CLON,
-        KC_NO,    KC_N,            KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_RO,
-                  LT(SYMB,KC_P1),  JA_ENVL,  KC_NO,   KC_NO,   KC_NO,
+        KC_RGHT,  KC_6,            KC_7,            KC_8,    KC_9,    KC_0,     KC_BSPC,
+        KC_NO,    KC_Y,            KC_U,            KC_I,    KC_O,    KC_P,     KC_MINS,
+                  KC_H,            KC_J,            KC_K,    KC_L,    KC_SCLN,  JA_CLON,
+        KC_NO,    KC_N,            KC_M,            KC_COMM, KC_DOT,  KC_SLSH,  KC_RO,
+                  LT(SYMB,KC_P1),  MEH_T(JA_ENVL),  KC_NO,   KC_NO,   KC_NO,
 
         TG(SYMB), CTL_T(KC_ESC),
         KC_PGUP,
