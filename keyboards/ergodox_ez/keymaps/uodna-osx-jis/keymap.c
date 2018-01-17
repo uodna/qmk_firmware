e0c28a42db221374b09fb256f9ef21a1b69e4c46
uodna <uodna0@gmail.com>
2018-01-17 12:17:02 +0900

    remove unused keys

--- keyboards/ergodox_ez/keymaps/uodna-osx-jis/keymap.c
+++ keyboards/ergodox_ez/keymaps/uodna-osx-jis/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |  1!  |  2"  |  3#  |  4$  |  5%  | LEFT |           | RIGHT|  6&  |  7'  |  8(  |  9)  |  0   |   BS   |
+ * |        |  1!  |  2"  |  3#  |  4$  |  5%  |      |           |      |  6&  |  7'  |  8(  |  9)  |  0   |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   -=   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -46,37 +46,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |      |      |      | /Alt |      |                                       | /L1  |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | BS   |  L2  |       |  L1  |Ctrl/Esc|
+ *                                        |      |  L2  |       |  L1  |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Del  |       | PgUp |        |      |
+ *                                 |      |      |      |       |      |        |      |
  *                                 | Space|      |------|       |------|  Meh   |Enter |
- *                                 |      |      | End  |       | PgDn |        |      |
+ *                                 |      |      |      |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,   KC_1,   KC_2,   KC_3,           KC_4,     KC_5,     KC_LEFT,
-        KC_TAB,   KC_Q,   KC_W,   KC_E,           KC_R,     KC_T,     KC_NO,
+        KC_NO,    KC_1,   KC_2,   KC_3,           KC_4,     KC_5,  KC_NO,
+        KC_TAB,   KC_Q,   KC_W,   KC_E,           KC_R,     KC_T,  KC_NO,
         KC_LCTL,  KC_A,   KC_S,   KC_D,           KC_F,     KC_G,
-        KC_LSFT,  KC_Z,   KC_X,   KC_C,           KC_V,     KC_B,     KC_NO,
+        KC_LSFT,  KC_Z,   KC_X,   KC_C,           KC_V,     KC_B,  KC_NO,
         KC_NO,    KC_NO,  KC_NO,  ALT_T(KC_ESC),  KC_LGUI,
 
-                                                            KC_BSPC,  TG(MDIA),
-                                                                      KC_DEL,
-                                                   KC_SPC,  KC_NO,    KC_END,
+                                                           KC_NO,  TG(MDIA),
+                                                                   KC_NO,
+                                                  KC_SPC,  KC_NO,  KC_NO,
 
         // right hand
-        KC_RGHT,  KC_6,            KC_7,     KC_8,    KC_9,    KC_0,     KC_BSPC,
-        KC_NO,    KC_Y,            KC_U,     KC_I,    KC_O,    KC_P,     KC_MINS,
-                  KC_H,            KC_J,     KC_K,    KC_L,    KC_SCLN,  JA_CLON,
-        KC_NO,    KC_N,            KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_RO,
-                  LT(SYMB,KC_P1),  JA_ENVL,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,  KC_6,            KC_7,     KC_8,    KC_9,    KC_0,     KC_NO,
+        KC_NO,  KC_Y,            KC_U,     KC_I,    KC_O,    KC_P,     KC_MINS,
+                KC_H,            KC_J,     KC_K,    KC_L,    KC_SCLN,  JA_CLON,
+        KC_NO,  KC_N,            KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_RO,
+                LT(SYMB,KC_P1),  JA_ENVL,  KC_NO,   KC_NO,   KC_NO,
 
-        TG(SYMB), CTL_T(KC_ESC),
-        KC_PGUP,
-        KC_PGDN,  KC_MEH,  KC_ENT
+        TG(SYMB),  KC_NO,
+        KC_NO,
+        KC_NO,     KC_MEH,  KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
