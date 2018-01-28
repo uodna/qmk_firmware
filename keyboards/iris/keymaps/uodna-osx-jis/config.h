935cf0c66940d2476a11d9ec376507ec2e65110c
uodna <uodna0@gmail.com>
2018-01-29 01:36:51 +0900

    configure tapping

--- keyboards/iris/keymaps/uodna-osx-jis/config.h
+++ keyboards/iris/keymaps/uodna-osx-jis/config.h
@@ -31,6 +31,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#undef  TAPPING_TERM
+#define TAPPING_TERM 100
+#define RETRO_TAPPING
+#define PERMISSIVE_HOLD
+
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 12
